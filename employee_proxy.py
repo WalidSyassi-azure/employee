d649a795c67808cf9607b0dd29bd4b963d6ccb4e
WalidSyassi-azure <syassisyassi@gmail.com>
2025-11-09 17:06:59 +0100

    Update employee_proxy.py

--- employee_proxy.py
+++ employee_proxy.py
@@ -83,7 +83,6 @@ log("=" * 80)
 
 odoo_session = requests.Session()
 odoo_session.headers.update({
-    # A common Chrome UA
     "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                    "AppleWebKit/537.36 (KHTML, like Gecko) "
                    "Chrome/122.0.0.0 Safari/537.36"),
